d50d15a4e66b89c443641b297d90d4fd824f7f82
Balázs Kovács <balazs.kovacs@stylersonline.com>
2020-04-28 10:18:49 +0200

    Manage basket

--- basket/src/app.js
+++ basket/src/app.js
@@ -1,9 +1,22 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
 const port = 80;
 
+let basket = {};
+
 app.route('/').get((req, res) => res.json({ service: 'basket' }));
+app.route('/add/:id').post((req, res) => {
+  basket[req.params.id] = {
+    count: basket[req.params.id] ? basket[req.params.id].count + 1 : 1
+  };
+  return res.json(basket);
+});
+app.route('/show').get((req, res) => res.json(basket));
+app.route('/clear').delete((req, res) => {
+  basket = {};
+  return res.json(basket);
+});
 
 app.listen(port, function() {
   console.log('Server started on port: ' + port);
--- gateway/src/app.js
+++ gateway/src/app.js
@@ -4,12 +4,12 @@ const axios = require('axios');
 const app = express();
 const port = 80;
 
-const fetch = async url => {
+const fetch = async (url, method, data) => {
   try {
-    const res = await axios.get(url);
+    const res = await axios.request({ url, method: method || 'get', data });
     return res.data;
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return { error };
   }
 }
@@ -17,6 +17,15 @@ const fetch = async url => {
 app.route('/').get((req, res) => res.json({ service: 'gateway' }));
 
 app.route('/basket').get(async (req, res) => res.json(await fetch('http://basket')));
+app.route('/basket/add/:id').post(
+  async (req, res) => res.json(await fetch(`http://basket/add/${req.params.id}`, 'post'))
+);
+app.route('/basket/show').get(
+  async (req, res) => res.json(await fetch('http://basket/show'))
+);
+app.route('/basket/clear').delete(
+  async (req, res) => res.json(await fetch('http://basket/clear', 'delete'))
+);
 
 app.route('/catalog').get(async (req, res) => res.json(await fetch('http://catalog')));
 app.route('/catalog/stock').get(async (req, res) => res.json(await fetch('http://catalog/stock')));
